5180a6794a96f629a83c84c6684c66baea3daf4c
박준현 <me@4rne5.dev>
2024-05-16 15:50:44 +0900

    [EDIT] Edit LaunchScreen +1
    - Edit Launchscreen
    - Edit LoginView TextInput Color Style

---
+++ Palette/Sources/Shared/Components/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  Palette
+//
+//  Created by 4rNe5 on 5/16/24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    TabBar()
+}
