628a64d2b089b540f22aea90bc311f0c17e6af0c
박준현 <me@4rne5.dev>
2024-07-11 01:57:22 +0900

    Add ContextMenu On Text for Fast Copy

--- Palette/Sources/Shared/Components/MessageBubble.swift
+++ Palette/Sources/Shared/Components/MessageBubble.swift
@@ -9,16 +9,15 @@ struct MessageBubble: View {
     let success_alert = Alert(title: "이미지 저장 완료!",
                       message: "이미지가 사진첩에 저장되었어요!",
                       dismissButton: .default("확인"))
-    
+
     private var formattedTime: String {
-        print(message.date)
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
         formatter.dateFormat = "a h:mm"
         return formatter.string(from: message.date)
     }
-    
+
     var body: some View {
         HStack(alignment: .bottom, spacing: 8) {
             if message.isAi {
@@ -32,7 +31,7 @@ struct MessageBubble: View {
         }
         .padding(.vertical, 4)
     }
-    
+
     private var messageContent: some View {
         Group {
             if message.resource == .IMAGE {
@@ -66,16 +65,24 @@ struct MessageBubble: View {
                 Text(message.message)
                     .font(.custom("SUIT-Medium", size: 14))
                     .padding(10)
+                    .contextMenu {
+                        Button(action: {
+                            UIPasteboard.general.string = message.message
+                        }) {
+                            Text("복사")
+                            Image(systemName: "doc.on.doc")
+                        }
+                    }
             }
         }
         .background(message.isAi ? Color.gray.opacity(0.2) : Color("AccentColor"))
         .foregroundColor(message.isAi ? .black : .white)
         .cornerRadius(13)
     }
-    
+
     private var timeText: some View {
         Text(formattedTime)
-            .font(.caption2)
+            .font(.custom("SUIT-Regular", size: 11))
             .foregroundColor(.gray)
     }
 }
