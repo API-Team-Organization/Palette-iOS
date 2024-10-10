4d91531d2de52d18e197b7bbbc6ac8e7edf5f87e
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-10-10 21:01:25 +0900

    ADD :: TODO

--- Palette/Sources/PaletteIosAppApp.swift
+++ Palette/Sources/PaletteIosAppApp.swift
@@ -3,14 +3,15 @@ import FlowKit
 
 @main
 
-// TODO : Token 인증 로직 구현
-// TODO : 여기서 토큰 여부에 따라 즉시 네비게이션 구현
-// TODO : 채팅 컴포넌트 제작 (아이디 맵핑, 넘겨서 채팅 조회 및 SSE 연결)
-// TODO : Server-Sent-Event 핸들러 제작
-// TODO : 채팅뷰 만들기
-// TODO : 로딩화면 처리
-// TODO : 화면 전환에 토큰갱신 API 삽입
-// TODO : 로그인 과정 Exception Case 디양화
+// TODO :: Token 인증 로직 구현
+// TODO :: 여기서 토큰 여부에 따라 즉시 네비게이션 구현
+// TODO :: 채팅뷰 통신구조 변경 (Feat @jombidev)
+// TODO :: 채팅뷰 하단 입력파트 입력 타입따라 다양화 (Grid, Picker)
+// TODO :: 이미지 로딩 파트 개선
+// TODO :: 채팅 Pageing
+// TODO :: 로딩화면 처리
+// TODO :: 화면 전환에 토큰갱신 API 삽입
+// TODO :: 로그인 과정 Exception Case 디양화
 
 struct PaletteIosAppApp: App {
     var body: some Scene {
