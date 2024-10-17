a76407330b84c4d64db8a54281cf19274df48ab7
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-10-17 12:09:36 +0900

    CHANGE :: Pakage 'FlowKit'
    - Fork FlowKit for Custom For Palette

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
          .package(url: "https://github.com/Alamofire/Alamofire", from: "5.9.1"),
          .package(url: "https://github.com/rive-app/rive-ios", from: "5.11.2"),
          .package(url: "https://github.com/onevcat/Kingfisher", from: "7.0.0"),
-         .package(url: "https://github.com/Mercen-Lee/FlowKit", .branch("main")),
+         .package(url: "https://github.com/4rNe5/FlowKit", .branch("main")),
          .package(url: "https://github.com/CSolanaM/SkeletonUI", .branch("master")),
     ]
 )
